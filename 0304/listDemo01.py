f2b30770c7ba49c7617f5fbd5ba3d05305d15dfd
某 康 <landuocat@163.com>
2022-03-04 14:38:35 +0800

    python列表学习

---
+++ 0304/listDemo01.py
@@ -0,0 +1,10 @@
+"""
+    定义和使用列表
+        在Python中，列表是由一系元素按特定顺序构成的数据序列，这样就意味着定义一个列表类型的变量，可以保存多个数据，而且允许有重复的数据。
+        列表也是一种结构化的、非标量类型，操作一个列表类型的变量，除了可以使用运算符还可以使用它的方法。
+"""
+# 列表元素的遍历
+items = ['python','java','go','kotlin']
+print(len(items))
+for index in range(len(items)):
+    print(items[index],index)
